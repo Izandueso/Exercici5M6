ca88959b6dd4c5a902b9fe35a7d7276730888b5a
Alumne <Alumne@104-PC11>
2020-01-24 17:45:46 +0100

    commito

--- src/Comprobarxml.java
+++ src/Comprobarxml.java
@@ -2,12 +2,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-import javax.lang.model.element.Element;
+import org.w3c.dom.Element;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.soap.Node;
+import org.w3c.dom.Node;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
@@ -27,28 +29,63 @@ public class Comprobarxml {
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document doc = dBuilder.parse(file);
+		
+		
 
 		//per obtenir el node arrel
 		org.w3c.dom.Element nodeArrel = doc.getDocumentElement();
-		NodeList nodeList = doc.getDocumentElement().getChildNodes();
+		NodeList nodeList = doc.getChildNodes();
 		
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node polla = nodeList.item(i);
+			Element caca = (Element) nodeList.item(i);
+			System.out.println(caca);
+			if(nodeList.item(i).equals()){
+//				Element p40000 = doc.createElement("alumne");
+//				p40000.setIdAttribute("id", true);
+				System.out.println("si");
+			}
+		}
+		
+		//Menu
 		System.out.println("1- Crear | 2- Modificar | 3- Eliminat | 4- Afegir atr | 5- Eliminar atr | 6- Modificar atr: ");
 		int menu = teclado.nextInt();
 		
 		if (menu == 1){
 			
+			//Busquem node
+			System.out.println("Introdueix una id: ");
+			String node = teclado.next();
+			Element c = doc.getElementById(node);
+			System.out.println(c);
+			
 			//Creem elements
 			System.out.println("Introdueix el element que vols crear: ");
 			String element = teclado.next();
-			org.w3c.dom.Element p = doc.createElement(element);
-			System.out.println(p);
+			c = doc.createElement(element);
+			System.out.println(c);
 			
 		}else if(menu == 2){
+			System.out.println("Introdueix element que vols modificar: ");
+			String elementEliminar = teclado.next();
+			Element eliminat = doc.createElement(elementEliminar);
 			
+			System.out.println("Introdueix el element el qual sustituira: ");
+			String element = teclado.next();
+			Element p = doc.createElement(element);
+			System.out.println(p);
+			
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				org.w3c.dom.Node temporal = nodeList.item(i);
+				if(temporal.equals(eliminat)){
+					temporal.getParentNode().removeChild(eliminat);
+					temporal.getParentNode().setNodeValue(element);
+				}
+			}
 		}else if(menu == 3){
 			System.out.println("Introdueix element que vols eliminar: ");
 			String elementEliminar = teclado.next();
-			org.w3c.dom.Element eliminat = doc.createElement(elementEliminar);
+			Element eliminat = doc.createElement(elementEliminar);
 			
 			for (int i = 0; i < nodeList.getLength(); i++) {
 				org.w3c.dom.Node temporal = nodeList.item(i);
@@ -59,6 +96,24 @@ public class Comprobarxml {
 			
 		}else if (menu == 4){
 			
+			System.out.println("Introdueix element al que introduirli els nous atributs:");
+			String element2 = teclado.next();
+			Element p2 = doc.createElement(element2);
+			
+			System.out.println("Introdueix name: ");
+			String name = teclado.next();
+			
+			System.out.println("Introdueix value: ");
+			String value = teclado.next();
+			
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				org.w3c.dom.Node temporal2 = nodeList.item(i);
+				if (temporal2.equals(p2)){
+					p2.setAttribute(name, value);
+				}
+			}
+			
+			
 		}else if(menu == 5){
 			
 		}else if(menu == 6){
@@ -67,11 +122,7 @@ public class Comprobarxml {
 			
 		}
 		
-		//Busquem node
-		System.out.println("Introdueix un node per cercar: ");
-		String node = teclado.next();
-		org.w3c.dom.Element c = doc.getElementById(node);
-		System.out.println(c);
+	
 		
 		//Creem un node de text
 		System.out.println("Introdueix el element que vols crear: ");
@@ -79,21 +130,12 @@ public class Comprobarxml {
 		Text text = doc.createTextNode(elementText);
 		System.out.println(text);
 		
-		//Introduim elnode que volem adquirir els atributs
-		System.out.println("Introduex element amb el qual vols treballar: ");
-		String elementNode = teclado.next();
-		org.w3c.dom.Element elementNodeVar = doc.createElement(elementNode);
 		
-		System.out.println("Introdueix node per obtenir atributs: ");
-		String nodeAtribut = teclado.next();
-		
-		String atribut = elementNodeVar.getAttribute(nodeAtribut);
-		System.out.println(atribut);
 		
 		
 		
 		for (int i = 0; i < nodeList.getLength(); i++) {
-			imprimirDadesXml((org.w3c.dom.Node)nodeList.item(i));
+			//imprimirDadesXml((org.w3c.dom.Node)nodeList.item(i));
 			
 		}	
 		
